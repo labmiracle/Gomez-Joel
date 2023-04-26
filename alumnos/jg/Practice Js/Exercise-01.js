5d9293df8d705d9a306aade97749df13aed827b0
joelgomezok <joelgomez995@gmail.com>
2023-04-25 21:14:49 -0300

    Exercise-1

---
+++ alumnos/jg/Practice Js/Exercise-01.js
@@ -0,0 +1,29 @@
+const names = [
+    "Hole-in-one!",
+    "Eagle",
+    "Birdie",
+    "Par",
+    "Bogey",
+    "Double Bogey",
+    "Go Home!",
+  ];
+  
+  function golfScore(par, strokes) {
+    
+    if (typeof par !== 'number' || typeof strokes !== 'number') {
+      return 'One of the values entered is not a number';
+    }
+    if (strokes < 1 || par < 1) {
+      return 'The information entered is incorrect';
+    }
+  
+    return (strokes == 1) ? names[0] :
+      (strokes <= par - 2) ? names[1] :
+      (strokes == par - 1) ? names[2] :
+      (strokes == par) ? names[3] :
+      (strokes == par + 1) ? names[4] :
+      (strokes == par + 2) ? names[5] :
+      names[6];
+  }
+  
+  console.log(golfScore(5, 4));
